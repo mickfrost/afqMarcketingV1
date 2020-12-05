d0cda0dbcf0a30cc30369658fc37c93c5591521b
Shehan Wijethunga <pwshehan@gmail.com>
2020-12-05 06:51:14 +0530

    Stage 1

--- README.md
+++
@@ -1,8 +0,0 @@
-# afqMarcketingV1
-
-WHAT WE OFFER – shehan  
-CONTENT
-https://afqmarketing.com/what-we-offer/
-
-BASE
-https://codepen.io/ReGGae/pen/bmyYEj
--- script.js
+++ script.js
@@ -3,53 +3,53 @@ class Slider {
     this.bindAll();
 
     this.vert = `
-      varying vec2 vUv;
-      void main() {
-        vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-      `;
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+    `;
 
     this.frag = `
-      varying vec2 vUv;
-  
-      uniform sampler2D texture1;
-      uniform sampler2D texture2;
-      uniform sampler2D disp;
-  
-      uniform float dispPower;
-      uniform float intensity;
-  
-      uniform vec2 size;
-      uniform vec2 res;
-  
-      vec2 backgroundCoverUv( vec2 screenSize, vec2 imageSize, vec2 uv ) {
-        float screenRatio = screenSize.x / screenSize.y;
-        float imageRatio = imageSize.x / imageSize.y;
-        vec2 newSize = screenRatio < imageRatio 
-            ? vec2(imageSize.x * (screenSize.y / imageSize.y), screenSize.y)
-            : vec2(screenSize.x, imageSize.y * (screenSize.x / imageSize.x));
-        vec2 newOffset = (screenRatio < imageRatio 
-            ? vec2((newSize.x - screenSize.x) / 2.0, 0.0) 
-            : vec2(0.0, (newSize.y - screenSize.y) / 2.0)) / newSize;
-        return uv * screenSize / newSize + newOffset;
-      }
-  
-      void main() {
-        vec2 uv = vUv;
-        
-        vec4 disp = texture2D(disp, uv);
-        vec2 dispVec = vec2(disp.x, disp.y);
-        
-        vec2 distPos1 = uv + (dispVec * intensity * dispPower);
-        vec2 distPos2 = uv + (dispVec * -(intensity * (1.0 - dispPower)));
-        
-        vec4 _texture1 = texture2D(texture1, distPos1);
-        vec4 _texture2 = texture2D(texture2, distPos2);
-        
-        gl_FragColor = mix(_texture1, _texture2, dispPower);
-      }
-      `;
+    varying vec2 vUv;
+
+    uniform sampler2D texture1;
+    uniform sampler2D texture2;
+    uniform sampler2D disp;
+
+    uniform float dispPower;
+    uniform float intensity;
+
+    uniform vec2 size;
+    uniform vec2 res;
+
+    vec2 backgroundCoverUv( vec2 screenSize, vec2 imageSize, vec2 uv ) {
+      float screenRatio = screenSize.x / screenSize.y;
+      float imageRatio = imageSize.x / imageSize.y;
+      vec2 newSize = screenRatio < imageRatio 
+          ? vec2(imageSize.x * (screenSize.y / imageSize.y), screenSize.y)
+          : vec2(screenSize.x, imageSize.y * (screenSize.x / imageSize.x));
+      vec2 newOffset = (screenRatio < imageRatio 
+          ? vec2((newSize.x - screenSize.x) / 2.0, 0.0) 
+          : vec2(0.0, (newSize.y - screenSize.y) / 2.0)) / newSize;
+      return uv * screenSize / newSize + newOffset;
+    }
+
+    void main() {
+      vec2 uv = vUv;
+      
+      vec4 disp = texture2D(disp, uv);
+      vec2 dispVec = vec2(disp.x, disp.y);
+      
+      vec2 distPos1 = uv + (dispVec * intensity * dispPower);
+      vec2 distPos2 = uv + (dispVec * -(intensity * (1.0 - dispPower)));
+      
+      vec4 _texture1 = texture2D(texture1, distPos1);
+      vec4 _texture2 = texture2D(texture2, distPos2);
+      
+      gl_FragColor = mix(_texture1, _texture2, dispPower);
+    }
+    `;
 
     this.el = document.querySelector(".js-slider");
     this.inner = this.el.querySelector(".js-slider__inner");
@@ -61,11 +61,7 @@ class Slider {
     this.clock = null;
     this.camera = null;
 
-    this.images = [
-      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/58281/bg1.jpg",
-      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/58281/bg2.jpg",
-      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/58281/bg3.jpg",
-    ];
+    this.images = ["img/slide01.jpg", "img/slide02.jpg", "img/slide03.jpg"];
 
     this.data = {
       current: 0,
@@ -105,6 +101,7 @@ class Slider {
       TweenMax.set(txt, {
         alpha: 0.25,
       });
+
       TweenMax.set(line, {
         scaleX: 0,
         transformOrigin: "left",
@@ -179,6 +176,7 @@ class Slider {
         texture2: { type: "t", value: this.textures[1] },
         disp: { type: "t", value: this.disp },
       },
+
       transparent: true,
       vertexShader: this.vert,
       fragmentShader: this.frag,
@@ -370,7 +368,18 @@ class Slider {
     tl.play();
   }
 
-  prevSlide() {}
+  prevSlide() {
+    if (this.state.animating) return;
+
+    this.state.animating = true;
+
+    this.transitionNext();
+
+    this.data.current =
+      this.data.current === this.data.total ? 0 : this.data.current - 1;
+    this.data.next =
+      this.data.current === this.data.total ? 0 : this.data.current - 1;
+  }
 
   nextSlide() {
     if (this.state.animating) return;
